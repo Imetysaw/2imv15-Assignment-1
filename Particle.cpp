997262b125782c98d22fbf796226c3c6a8e8038a
Ruud Andriessen <ruud.andriessen@gmail.com>
2017-05-03 10:57:08 +0200

    Initial project commit

---
+++ Particle.cpp
@@ -0,0 +1,32 @@
+#include "Particle.h"
+#if defined(_WIN32) || defined(WIN32)
+    #include <GL/glut.h>
+#else
+    #include <GLUT/glut.h>
+#endif
+
+Particle::Particle(const Vec2f & ConstructPos) :
+	m_ConstructPos(ConstructPos), m_Position(Vec2f(0.0, 0.0)), m_Velocity(Vec2f(0.0, 0.0))
+{
+}
+
+Particle::~Particle(void)
+{
+}
+
+void Particle::reset()
+{
+	m_Position = m_ConstructPos;
+	m_Velocity = Vec2f(0.0, 0.0);
+}
+void Particle::draw()
+{
+	const double h = 0.03;
+	glColor3f(1.f, 1.f, 1.f); 
+	glBegin(GL_QUADS);
+	glVertex2f(m_Position[0]-h/2.0, m_Position[1]-h/2.0);
+	glVertex2f(m_Position[0]+h/2.0, m_Position[1]-h/2.0);
+	glVertex2f(m_Position[0]+h/2.0, m_Position[1]+h/2.0);
+	glVertex2f(m_Position[0]-h/2.0, m_Position[1]+h/2.0);
+	glEnd();
+}
